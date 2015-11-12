9a01d79b083ac9816f8dc00a8fb0658f9bdbd906
技术部－张劲松 <admin@admindeMacBook-Air.local>
2015-11-12 15:32:04 +0800

    Initial Commit

---
+++ xiaozhunongchang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xiaozhunongchang
+//
+//  Created by 技术部－张劲松 on 15/11/12.
+//  Copyright (c) 2015年 技术部－张劲松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xiaozhunongchang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xiaozhunongchang
+//
+//  Created by 技术部－张劲松 on 15/11/12.
+//  Copyright (c) 2015年 技术部－张劲松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
